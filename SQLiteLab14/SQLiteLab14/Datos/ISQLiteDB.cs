bf0a69d29cc30ab797990e1587b1001198e2699f
alvarofabian0432 <alvaro.motta@tecsup.edu.pe>
2021-06-08 20:30:25 -0500

    first commit

---
+++ SQLiteLab14/SQLiteLab14/Datos/ISQLiteDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace SQLiteLab14.Datos
+{
+    public interface ISQLiteDB
+    {
+        SQLiteAsyncConnection GetConnection();
+    }
+}
